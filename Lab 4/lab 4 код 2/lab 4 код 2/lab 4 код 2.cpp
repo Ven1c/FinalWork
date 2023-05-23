3d3832e4584b31186912691362a556cd213279b4
Ven1c <134434340+Ven1c@users.noreply.github.com>
2023-05-23 21:01:57 +0300

    Add files via upload

---
+++ Lab 4/lab 4 код 2/lab 4 код 2/lab 4 код 2.cpp
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int A[5];
+    int B[6];
+    int C[11];
+    int V[11];
+    int o=0;
+    
+    for (int i = 0; i <= 4; i++) {
+        A[i] =  -1*rand() ;
+        
+    }
+    for (int i = 0; i <= 5; i++) {
+        B[i] = rand();
+
+    }
+    for (int i = 0; i <= 4; i++) {
+        C[i] = A[i];
+       
+    }
+    for (int i = 0; i <= 5; i++) {
+        C[i+5] = B[i];
+    }
+    for (int i = 0; i <= 10; i++) {
+        printf("Massive= % d\n", C[i]);
+    }
+
+    for (int i = 0; i <= 11; i++) {
+        if (C[i] > 0) {
+            printf(" +=% d\n", C[i]);
+        } else {
+            V[o]=C[i]  ;
+            o++;
+        }
+    }
+    for (int i = 0; i <= 5; i++) {
+        printf(" % d\n", V[i]);
+    }
+}
+
+
---
+++ Lab 4/lab 4Код 1/lab 4/lab 4.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    
+    float sr = 0;
+    float sum =0;
+    int arr[10];
+    for (int i = 0; i <= 9; i++) {
+        arr[i] = 1+rand()%10;
+        printf(" % d\n", arr[i]);
+        sum = sum + arr[i];
+    }
+    sr = sum / 10 ;
+    printf(" % f\n", sr);
+}
+
